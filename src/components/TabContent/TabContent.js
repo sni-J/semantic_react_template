37ddd56772a8675b0dcaf36baec2b4e30ebf0b0f
sni-J <wjl0316@naver.com>
2019-08-05 00:41:21 +0900

    TabContent simplify

--- src/components/TabContent/TabContent.js
+++ src/components/TabContent/TabContent.js
@@ -1,51 +1,37 @@
 import React, { Component } from 'react'
 import {
-  Card,
-  Divider,
-  Tab,
+  Container
 } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
+class HomeContent extends Component {
+  render(){
+    return (
+      <Container fluid>
+        <p>Home Content</p>
+      </Container>
+    )
+  }
+}
+
 export default class TabContent extends Component {
+  components = {
+      'home': HomeContent
+  }
+
   render(){
     const { activeItem } = this.props
+    const TagName = this.components[activeItem]
 
-    switch (activeItem) {
-      case 'home':
-        return (
-          <Card.Group>
-            <Card>
-              <Card.Content>
-                <Card.Header>Home</Card.Header>
-                <Card.Meta>Meta</Card.Meta>
-                <Divider />
-                <Card.Description>
-                  <p>Okay, This is Home</p>
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                Extra Area <br/>
-                Nicely divided
-              </Card.Content>
-            </Card>
-            <Card
-              header="Home"
-              meta="Meta"
-              description="Okay, This is home"
-              extra="Extra Area Nicely Divided"
-            />
-          </Card.Group>
-        )
-      case 'messages':
-        return (
-          <Tab.Pane>
-            <p>Message Here</p>
-          </Tab.Pane>
-        )
-      case 'friends':
-        return (<p>friends Here</p>)
-      default:
-        return (<p>Not Found, But Not 404. Maybe under construction.</p>)
+    if(TagName){
+      return (
+        <TagName />
+      )
+    }
+    else{
+      return (
+        <p>Not Found, But Not 404. Maybe under construction.</p>
+      )
     }
   }
 }
