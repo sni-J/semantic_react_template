db763b9e5417e9ff38ff9fa6e0a38b8158fe0613
sni-J <wjl0316@naver.com>
2019-08-06 01:53:13 +0900

    Home contents

--- src/components/TabContent/TabContent.js
+++ src/components/TabContent/TabContent.js
@@ -1,44 +1,55 @@
 import React, { Component } from 'react'
 import {
-  Container
+  Container,
+  Divider,
+  Button,
 } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import axios from 'axios'
 
 class HomeContent extends Component {
+  state = {stargazers_count: 'Loading...', forks_count: 'Loading...'}
+
+  getGithubInfo =
+    axios.get('https://api.github.com/repos/sni-J/semantic_react_test')
+    .then(({data})=>{
+      console.log(data.svn_url);
+      const {stargazers_count, forks_count, svn_url} = data
+      this.setState({...{stargazers_count, forks_count, svn_url}})
+    })
+    .catch((e) => console.log('getGithubInfo: '+e))
+
+  fetchData = async () => {
+    await this.getGithubInfo
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render(){
     return (
       <Container>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus pretium erat id molestie. Suspendisse varius, dui vitae laoreet tempor, justo dui porttitor mauris, id tempor nunc diam ut ante. Mauris ut facilisis ex. Morbi ut mattis odio. Cras bibendum blandit mi, ac commodo mi euismod aliquam. Nam bibendum vulputate magna, quis tincidunt ligula iaculis id. Praesent convallis ultrices arcu, eu convallis dolor blandit ac.
-
-        In eleifend, ex ultricies bibendum rhoncus, turpis erat hendrerit risus, sit amet hendrerit mauris arcu a augue. Vestibulum dictum magna at mauris venenatis tempus. Sed et metus velit. Etiam tempor ligula nibh, facilisis luctus velit pretium vitae. Nunc convallis sem a sem vestibulum, tristique aliquet felis egestas. Nullam pretium, urna sed pulvinar hendrerit, nibh ex luctus mauris, vel lobortis est justo id eros. Sed id est sed sem ullamcorper ornare eget vitae eros.
-
-        Donec tempor magna eu nisl luctus, vitae volutpat orci pharetra. Quisque pulvinar ullamcorper ligula. Phasellus faucibus finibus dignissim. Donec ac dolor consectetur, sodales elit id, pharetra libero. Mauris quis hendrerit arcu. In sit amet libero quis velit mattis condimentum non vitae est. Etiam rutrum erat nec velit commodo, maximus malesuada sapien eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent eget aliquam odio. Duis tempus metus non quam hendrerit ultricies. Suspendisse sit amet nulla id felis hendrerit sollicitudin. Aenean sed sollicitudin ligula. Sed sagittis viverra finibus. Ut vehicula eros orci, non mollis neque lobortis eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-
-        Praesent mattis pharetra diam, eget finibus magna ullamcorper quis. In est felis, tincidunt vitae luctus sit amet, fringilla id nisi. Pellentesque ac pellentesque ligula. Nullam rutrum, enim sed pharetra dapibus, ligula nisl laoreet neque, nec fermentum urna nibh eget turpis. Mauris quis lacinia libero, id iaculis sapien. Aliquam erat volutpat. Phasellus massa turpis, ornare suscipit ligula in, semper accumsan arcu. In malesuada justo diam, eu vehicula neque tincidunt sit amet. Curabitur ultricies tincidunt neque vel molestie. Nullam ac consequat ante, vitae blandit urna. Aliquam in ullamcorper quam.
-
-        Cras cursus iaculis arcu, at aliquet est faucibus at. Cras lobortis ante lorem, eu rutrum arcu condimentum eu. Nunc magna massa, finibus eget scelerisque a, efficitur suscipit neque. Aenean turpis mauris, posuere eget est et, consequat tincidunt tortor. Fusce mollis massa eu consequat consectetur. Pellentesque et ornare mauris, at convallis libero. Vestibulum laoreet mi erat, non facilisis magna scelerisque ac.
-
-        Quisque ut sagittis dui. Sed vel sollicitudin orci, ut consectetur ante. Ut sit amet vestibulum urna. Praesent rutrum quam a lorem tincidunt venenatis. Nulla id mi sed sapien dignissim eleifend. Mauris ut gravida est. Duis non suscipit justo, sed fermentum nibh. Nullam placerat consectetur purus, id scelerisque lacus porttitor id. Aliquam iaculis nibh sapien, at scelerisque lorem facilisis feugiat. Aliquam vitae libero id risus aliquam luctus. Vestibulum varius tortor eget leo vehicula tempus. Cras tellus erat, vestibulum quis dolor sit amet, viverra aliquet leo.
-
-        Donec porta pretium consequat. Aliquam sed eleifend diam. Aliquam turpis nunc, mattis in mi in, rutrum venenatis felis. Fusce sed facilisis sapien. Vestibulum fermentum aliquet pellentesque. Curabitur ultrices velit quis massa mollis, non cursus diam tempor. Vivamus ex ipsum, iaculis eu pulvinar sit amet, facilisis nec tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent ut augue non nibh interdum vulputate. Nunc blandit condimentum efficitur. Phasellus pharetra, nibh eu consequat lacinia, odio urna mollis felis, rutrum euismod purus mauris vitae metus. Aenean ultrices faucibus est, luctus lobortis tortor iaculis quis. Phasellus molestie ligula et congue efficitur. Duis tellus diam, interdum a leo eget, sollicitudin ultricies magna.
-
-        Fusce sed ipsum a nulla consectetur dignissim ac vitae augue. Suspendisse maximus est diam, et tempus dolor sollicitudin vitae. Aenean vitae urna dignissim, aliquet orci nec, suscipit mi. Morbi viverra odio ac ex pellentesque, ac placerat orci suscipit. Proin ut scelerisque ex, eget aliquet dolor. Morbi finibus volutpat leo, a mollis tortor tempus a. Phasellus lacinia urna in arcu efficitur, quis condimentum libero rutrum. In egestas massa non ipsum facilisis aliquet. Vivamus quis magna eu est eleifend venenatis non id lacus. Integer eu risus vitae massa viverra hendrerit nec id massa. Cras dapibus metus eget sapien hendrerit laoreet.
-
-        Suspendisse scelerisque nisi ut tellus ullamcorper, fermentum aliquam ipsum blandit. Curabitur tellus odio, pulvinar et massa vel, cursus gravida velit. Phasellus at tristique ligula. Aenean interdum viverra varius. Donec vitae lectus sit amet dolor fermentum suscipit non id tortor. Nunc lacinia ac sapien id rutrum. Nam eu lobortis diam. Proin lectus nibh, tempor eget tempus sed, interdum ut mi. Nulla porttitor dui sed dolor volutpat tempor. Fusce urna nulla, faucibus et vulputate non, malesuada nec lectus. Ut porttitor vitae odio eget egestas. Mauris porttitor pretium laoreet. Quisque suscipit leo placerat, pharetra diam nec, posuere sapien. Donec bibendum orci risus. Sed nisl nulla, facilisis gravida tristique eu, consectetur eu odio. Sed ultrices eget diam sit amet lobortis.
-
-        Curabitur gravida mauris neque, id molestie metus consequat ac. Duis vitae imperdiet felis. Aliquam nec congue tortor. Integer sit amet dui justo. Phasellus nec est eu enim tincidunt malesuada a non metus. Vivamus dapibus leo ut diam vehicula aliquam vitae volutpat ipsum. Sed fringilla, orci sed egestas vehicula, magna sapien feugiat tellus, in commodo lacus tortor sit amet nunc. Maecenas cursus sapien eu justo congue condimentum.
-
-        Sed ut felis in lorem luctus gravida quis eget leo. Morbi dapibus volutpat venenatis. Aliquam condimentum velit quis turpis tincidunt, vitae hendrerit libero vulputate. Etiam vestibulum tristique eros at blandit. Nunc vestibulum condimentum orci ullamcorper blandit. Maecenas lobortis urna id ligula maximus posuere. Nulla consectetur, est eu feugiat pulvinar, justo augue sollicitudin neque, vel pretium massa lorem quis nisi.
-
-        Aenean sed leo orci. Suspendisse potenti. Aliquam sagittis condimentum est, in egestas lorem lobortis quis. Suspendisse sit amet neque ipsum. Nulla ut urna elit. Aenean et nisl nec magna commodo ornare. Integer purus odio, aliquet sit amet fermentum eu, dapibus eu urna. Suspendisse mollis augue porttitor erat bibendum vulputate. Maecenas sodales turpis sed nisl condimentum, ac rhoncus dolor tempus. Donec ipsum leo, sodales ac enim a, tempor dignissim turpis. Curabitur fringilla convallis malesuada. Mauris rutrum est nibh. Aenean dignissim semper mauris et ullamcorper. Mauris nulla massa, cursus ut lacinia eu, elementum a quam. Suspendisse potenti.
-
-        Sed semper enim vel tellus venenatis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In ex ligula, porta eu nibh at, venenatis suscipit ex. Sed consequat elit mauris, eu sollicitudin sapien ullamcorper vitae. Etiam semper aliquet condimentum. Sed id egestas purus. Morbi imperdiet eros a viverra scelerisque. Integer suscipit sodales sem vel rhoncus. Vestibulum massa diam, malesuada ac erat mattis, lacinia faucibus diam. Duis nunc erat, ornare a scelerisque quis, auctor non turpis.
-
-        Nam tempor interdum nunc, ut fringilla odio ornare id. Nam sed dictum lectus, a ultrices ex. In laoreet risus sit amet sollicitudin tristique. Nunc sagittis ante lectus, at lacinia tellus consectetur in. Proin suscipit sagittis velit, at hendrerit ligula malesuada nec. Duis sit amet maximus orci. Aenean accumsan, lorem eu interdum pharetra, lacus purus dapibus tellus, sit amet congue augue justo a mi. Maecenas viverra sem vitae fringilla congue.
-
-        Nunc interdum interdum sodales. Proin tempor enim ac ligula scelerisque ultrices. Vivamus lorem neque, suscipit sit amet diam eu, dapibus luctus leo. Aenean semper, velit nec vulputate semper, nisl odio ornare erat, eget convallis mauris tortor imperdiet turpis. Nullam sagittis nisi non sem auctor interdum. In vel urna ac ipsum efficitur commodo. Nulla nec elementum erat. Praesent egestas ligula vel lorem volutpat, sit amet vehicula odio fringilla. Vestibulum ut odio congue, lacinia urna a, tristique metus. Ut cursus tempor diam, sit amet elementum neque vulputate eget. Phasellus sed quam ut magna rutrum feugiat.
-        </p>
+        <h2 id="responsive-template-implemented-with-create-react-app--semantic-react">Responsive Template implemented with <a href="https://github.com/facebook/create-react-app">Create React App</a> + <a href="https://react.semantic-ui.com/">Semantic-react</a></h2>
+        <Divider />
+        <ul>
+          <li>Structure
+            <ul>
+              <li>
+                App
+                <ul>
+                  <li>Navbar: Responsive menu navigation bar</li>
+                  <li>TabContent: Contents changes along the selection of menu</li>
+                  <li>Footer: Footer contents Also Responsive</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <Divider />
+        <Button icon='github' color='black' as='a' href={this.state.svn_url} content='Visit Github'/>
+        <Button icon='star' color='black' label={{as: 'a', color:'black', basic: false, href: this.state.svn_url+'/stargazers', content: this.state.stargazers_count}} />
+        <Button icon='fork' color='black' label={{as: 'a', color:'black', basic: false, href: this.state.svn_url+'/network/members', content: this.state.forks_count}} />
       </Container>
     )
   }
